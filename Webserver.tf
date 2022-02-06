9144b12c92feb45c699c52e4f6978a5d040953ea
Timur-IT <timurdzhafarov734@gmail.com>
2022-02-06 14:46:21 +0300

    Added Webserver.tf

---
+++ Webserver.tf
@@ -0,0 +1,50 @@
+provider "aws" {}
+
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-011996ff98de391d1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  user_data = templatefile("user_data.sh.tpl", {
+    f_name = "Timur",
+    l_name = "Jafarov",
+    names  = ["Vasya", "Kolya", "Donald", "John"]
+  })
+
+  tags = {
+    Name = "Server"
+  }
+}
+
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "My first SG"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "WebServer Security Group"
+  }
+}
